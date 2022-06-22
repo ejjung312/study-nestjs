d1c6124284bb343d26355d96654be4f35a1491a7
jungeunji <ejjung312@gmail.com>
2022-06-22 20:34:18 +0900

    boards crud 구현중

---
+++ app/src/boards/boards.controller.ts
@@ -0,0 +1,21 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Board } from './boards.model';
+import { BoardsService } from './boards.service';
+
+@Controller('boards')
+export class BoardsController {
+  constructor(private boardService: BoardsService) {} // 프로퍼티가 자동으로 생성됨
+
+  @Get()
+  getAllBoard(): Board[] {
+    return this.boardService.getAllBoards();
+  }
+
+  @Post()
+  createBoard(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Board {
+    return this.boardService.createBoard(title, description);
+  }
+}
