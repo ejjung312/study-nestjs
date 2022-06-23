a192d1217a7d838658e58870b270c49abe8bf455
jungeunji <ejjung312@gmail.com>
2022-06-23 20:09:16 +0900

    DB설정 변경

--- app/src/configs/typeorm.config.ts
+++ app/src/configs/typeorm.config.ts
@@ -2,11 +2,11 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeORMConfig: TypeOrmModuleOptions = {
   type: 'mysql',
-  host: 'localhost',
+  host: '139.150.74.240',
   port: 3306,
-  username: 'postgres',
-  password: 'postgres',
-  database: 'board-app',
+  username: 'nestjs',
+  password: 'qwer1234!@#',
+  database: 'nestjsDB',
   entities: [__dirname + '/../**/*.entity.{js,ts}'], // 모든 엔티티를 포함
   synchronize: true, // 서비스가 실행되고 데이터베이스가 연결될 때 항상 데이터베이스가 초기화 됨
 };
